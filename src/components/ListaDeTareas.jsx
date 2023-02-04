4f3c32a2de9343d8a75fa49addeae5f86d336204
Daniel Alvarez <danielalvarezmoncada527@gmail.com>
2023-02-04 14:37:32 -0500

    proyecto finalizado

--- src/components/ListaDeTareas.jsx
+++ src/components/ListaDeTareas.jsx
@@ -17,6 +17,22 @@ function ListaDeTareas(){
     }
   };
 
+  const eliminarTarea = id =>{
+    const tareasActualizadas = tareas.filter(tarea => tarea.id !== id);
+    setTareas(tareasActualizadas);
+  };
+
+  const completarTarea = id => {
+    const tareasActualizadas = tareas.map(tarea => {
+      if(tarea.id === id){
+        tarea.completada = !tarea.completada;
+      }
+      return tarea;
+    });
+
+    setTareas(tareasActualizadas);
+  };
+
   return(
     <>
       <Formulario onSubmit={agregarTarea} />
@@ -28,6 +44,8 @@ function ListaDeTareas(){
               id={tarea.id}
               texto={tarea.texto}
               completada={tarea.completada}
+              completarTarea={completarTarea}
+              eliminarTarea={eliminarTarea}
             />
           )
         }
